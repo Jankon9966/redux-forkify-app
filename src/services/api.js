bc53e10a7347d2d9aba8de29733198f737f657bb
Janko Novakovic <licardian9996@gmail.com>
2023-08-09 20:29:13 +0200

    services

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getRecipes = async () => {
-    try {
-        const response = await axios.get("https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza");
-        return response.data.data.recipes;
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    const response = await axios.get(
+      "https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza"
+    );
+    return response.data.data.recipes;
+  } catch (error) {
+    console.log(error);
+  }
+};
